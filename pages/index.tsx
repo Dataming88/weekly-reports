5509c62d5f0a4dfad0e1ba3cb9e7085a583f2e9a
Quentin <103133682+gaoshengming88@users.noreply.github.com>
2023-04-04 15:36:49 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ const Home: NextPage = () => {
 
       <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/gaoshengming88/weekly_reports"
+          href="https://github.com/gaoshengming88/weekly-reports"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -193,7 +193,7 @@ const Home: NextPage = () => {
                 {t('privacyPolicy1')}
               <a
                 className="text-blue-200 hover:text-blue-400"
-                href="https://github.com/gaoshengming88/weekly_reports/blob/main/privacy.md"
+                href="https://github.com/gaoshengming88/weekly-reports/blob/main/privacy.md"
                 target="_blank"
                 rel="noopener noreferrer"
               >{' '}{t('privacyPolicy2')}</a>
